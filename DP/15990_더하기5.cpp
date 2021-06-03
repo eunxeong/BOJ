a749efdd01800295101efb51825d199ab50639af
eunxeong <1717197@naver.com>
2021-06-03 19:42:56 +0900

    0603 1차

---
+++ DP/15990_더하기5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+// 15990 1, 2, 3 ´õÇÏ±â 5
+
+int main(){
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int T;	cin >> T;
+	long long d[10001][4] = { 0, };
+	
+	d[1][1] = 1;
+	d[2][2] = 1;
+	d[3][1] = 1;
+	d[3][2] = 1;
+	d[3][3] = 1;
+	
+	for(int i = 4; i <= 10000; i++){
+		
+		d[i][1] = (d[i - 1][2] + d[i - 1][3]) % 1000000009;
+		d[i][2] = (d[i - 2][1] + d[i - 2][3]) % 1000000009;
+		d[i][3] = (d[i - 3][1] + d[i - 3][2]) % 1000000009;
+		
+	}
+
+	while(T--){
+		
+		int idx;	cin >> idx;
+		long long Answer = (d[idx][1] + d[idx][2]) % 1000000009;
+		Answer = (Answer + d[idx][3]) % 1000000009;
+		
+		cout << Answer << "\n";
+		
+	}
+	
+	return 0;
+	
+}
