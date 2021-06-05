8d8626f0bc592ff091ac4ae89efe71d1f259101b
eunxeong <1717197@naver.com>
2021-06-05 15:28:28 +0900

    시간초과 해결 x

--- DFS,BFS/16946_벽부수고이동하기4.cpp
+++ DFS,BFS/16946_벽부수고이동하기4.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
-#include <queue>
 #include <string.h>
+#include <queue>
 
 using namespace std;
 // 16946 벽 부수고 이동하기 4
 
 int N, M;
-int MAP[101][101];
-int ans[101][101] = { 0, };
-int zero[101][101] = { 0, };
-int zz[101];
-bool visited[101][101];
-
+int MAP[1001][1001];
+int ans[1001][1001];
+int zero[1001][1001] = { 0, };
 int zeroN = 0;
-int dir[4][2] = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
+int area[500001];
 
+bool visited[1001][1001] = { false, };
+bool check[500001];
+
+int dir[4][2] = { { 0, 1}, {0, -1}, {1, 0}, {-1, 0} };
 
 void bfs(int a, int b){
 	
-	zeroN++;
-	int cnt = 1;
-	
+	visited[a][b] = true;
+	zeroN++;//0칸 번호
+	zero[a][b] = zeroN;
+	 
 	queue<pair<int, int> > Q;
 	Q.push(make_pair(a, b));
-	zero[a][b] = zeroN;
+	
+	int cnt = 1;//0 개수 카운트
 	
 	while(!Q.empty()){
-		
+	
 		int k = Q.size();
 		while(k--){
-		
 			int x = Q.front().first;
 			int y = Q.front().second;
 			Q.pop();
-			
+	
 			for(int i = 0; i < 4; i++){
 				int nx = x + dir[i][0];
 				int ny = y + dir[i][1];
@@ -44,41 +46,45 @@ void bfs(int a, int b){
 				visited[nx][ny] = true;
 				zero[nx][ny] = zeroN;
 				cnt++;
+				Q.push(make_pair(nx, ny));
 			}
-		
 		}
-		
-	}
 	
-	zz[zeroN] = cnt;
+	}
+		
+	area[zeroN] = cnt;
 	
 }
 
-void dfs(int x, int y){
+void countAns(int x, int y){
 	
-	if (visited[x][y])	return;
-	visited[x][y] = true;
-//	cnt++;
+	memset(check, false, sizeof(check));
+	int cnt = 1;
 	
 	for(int i = 0; i < 4; i++){
-		
 		int nx = x + dir[i][0];
 		int ny = y + dir[i][1];
 		
 		if (nx < 0 || nx >= N || ny < 0 || ny >= M)	continue;
-//		if (visited[nx][ny] || MAP[nx][ny] != 0)	continue;
+		if (MAP[nx][ny])	continue;
 		
-		dfs(nx, ny);
+		if (check[zero[nx][ny]])	continue;
+		cnt += area[zero[nx][ny]];
+		check[zero[nx][ny]] = true;
+		
 	}
+	
+	ans[x][y] = cnt % 10;
 	
 }
 
 int main(){
 
 	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+	cin.tie(0);	cout.tie(0);
 	
 	cin >> N >> M;
+	
 	for(int i = 0; i < N; i++){
 		string s;	cin >> s;
 		for(int j = 0; j < M; j++)	MAP[i][j] = s[j] - '0';
@@ -92,33 +98,18 @@ int main(){
 	
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < M; j++){
-			cout << zz[zero[i][j]];
+			if (MAP[i][j] == 1){
+				countAns(i, j);
+			}
+		}
+	}
+
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < M; j++){
+			cout << ans[i][j];
 		}
 		cout << "\n";
 	}
-	
-	bool check[500] = {false, };
-	
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-			if (MAP[i][j] == 1){
-				int k = 1;
-				for(int k = 0; k < 4; k++){
-					int nx = i + dir[k][0];
-					int ny = j + dir[k][1];
-					memset(check, false, sizeof(check));
-					if (nx < 0 || nx >= N || ny < 0 || ny >= M)	continue;
-					if (MAP[nx][ny] == 0)
-			
-		}
-	}
-	
-//	for(int i = 0; i < N; i++){
-//		for(int j = 0; j < M; j++){
-//			cout << ans[i][j] % 10;
-//		}
-//		cout << "\n";
-//	}
 	
 	return 0;
 	
