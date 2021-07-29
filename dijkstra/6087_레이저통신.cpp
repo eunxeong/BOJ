e52bb977bdd33bc8a1352b8bc80ac60597cf579c
eunxeong <1717197@naver.com>
2021-07-29 16:24:46 +0900

    bfs+dijkstra

--- dijkstra/6087_레이저통신.cpp
+++ dijkstra/6087_레이저통신.cpp
@@ -5,70 +5,89 @@
 using namespace std;
 // 6087 레이저 통신
 
+int N, M;
+char map[101][101];
+bool check[101][101] = { false, };
+
+int start_x, start_y, end_x, end_y;
 int dir[4][2] = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
+
+int bfs(){
+
+    queue<pair<int, int> > Q;
+    Q.push(make_pair(start_x, start_y));
+
+    check[start_x][start_y] = true;
+    int cnt = -1;
+
+    while(!Q.empty()){
+
+        int k = Q.size();
+        cnt++;
+
+        while(k--){
+
+            int x = Q.front().first;
+            int y = Q.front().second;
+            Q.pop();
+            for(int i = 0; i < 4; i++){
+                
+                int t = 1;
+                while(1){
+
+                    int nx = x + dir[i][0]*t;
+                    int ny = y + dir[i][1]*t;
+
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= M) break;
+                    if (nx == end_x && ny == end_y) return cnt;//도착한거
+
+                    if (map[nx][ny] == '*'){
+                        break;//못 뚫고 가니까
+                    }
+
+                    if (!check[nx][ny]){//아직 넣었다면 당연히 넣고
+                        check[nx][ny] = true;
+                        Q.push(make_pair(nx, ny));
+                    }
+                    t++;//이칸은 들렸더라도 레이저가 닿는 그 다음칸은 안갔을 수도 있음
+                }
+            }
+
+        }
+    }
+
+    return -1;
+}
 
 int main(){
 
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int N, M;   cin >> M >> N;
-    char map[101][101];
-    vector<pair<int, int> > V;//C 위치
-    
+
+    cin >> M >> N;
+
+    int tmp = 0;
     for(int i = 0; i < N; i++){
         string s;   cin >> s;
         for(int j = 0; j < M; j++){
             map[i][j] = s[j];
-            if (map[i][j] == 'c'){
-                V.push_back(make_pair(i, j));
+            if (map[i][j] == 'C'){
+                if (tmp == 0){
+                    start_x = i;
+                    start_y = j;
+                    tmp++;
+                }
+                else{
+                    end_x = i;
+                    end_y = j;
+                }
             }
         }
     }
-	
-    int d[101][101];
-    int INF = 10000000;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++)  d[i][j] = INF;
-    }
 
-    priority_queue<pair<pair<int, int>, pair<int, int> > > pq;
-    d[V[0].first][V[0].second] = 0;
-    
-//    pq.push(make_pair(make_pair(0, -1), make_pair(V[0].first, V[0].second)));
-	pq.push(make_pair(make_pair(0, 0), make_pair(0, 0)));
+    int answer = bfs();
 
-//    while(!pq.empty()){
-//
-//        int x = pq.top().second.first;
-//        int y = pq.top().second.second;
-//        int cur = pq.top().first.second;//이전 방향
-//        int w = pq.top().first.first * -1;//지금까지 턴
-//
-//        pq.pop();
-//        if (d[x][y] < w)    continue;
-//
-//        for(int i = 0; i < 4; i++){
-//            int nx = x + dir[i][0];
-//            int ny = y + dir[i][1];
-//
-//            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-//            if (map[nx][ny] == '*') continue;
-//
-//            int nw = w;
-//            if (i != cur){
-//                if (cur != -1)   nw++;//전과 같은 방향이 아니고 처음 시작도 아니면 한번 꺾었다는거니까 nw++
-//            }
-//
-//            if (d[nx][ny] > nw){
-//                pq.push(make_pair(make_pair(-nw, i), make_pair(nx, ny)));
-//                d[nx][ny] = nw;
-//            }
-//        }
-//
-//    }
-//    cout << d[V[1].first][V[1].second];
-
+    cout << answer;
     return 0;
 
 }
